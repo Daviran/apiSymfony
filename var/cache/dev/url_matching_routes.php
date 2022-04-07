3764e9a60b0c62f27707cf1975215921cff30fdc
daviran <dbugnon@outlook.fr>
2022-04-07 09:39:12 +0200

    feat: authentification basically working.

--- var/cache/dev/url_matching_routes.php
+++ var/cache/dev/url_matching_routes.php
@@ -8,6 +8,7 @@
 return [
     false, // $matchHost
     [ // $staticRoutes
+        '/dashboard' => [[['_route' => 'dashboard', '_controller' => 'App\\Controller\\DashboardController::index'], null, null, null, false, false, null]],
         '/orders' => [[['_route' => 'app_orders', '_controller' => 'App\\Controller\\OrdersController::index'], null, null, null, false, false, null]],
         '/api/products' => [[['_route' => 'app_product', '_controller' => 'App\\Controller\\ProductController::index'], null, null, null, false, false, null]],
         '/api/product' => [[['_route' => 'project_new', '_controller' => 'App\\Controller\\ProductController::new'], null, ['POST' => 0], null, false, false, null]],
