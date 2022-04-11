17dea51de14c8d4332a7bd8b46ddc4aff191c566
daviran <dbugnon@outlook.fr>
2022-04-11 10:01:05 +0200

    wip: token auth for cart

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class UserController extends AbstractController
 {
@@ -28,4 +30,33 @@ class UserController extends AbstractController
             'controller_name' => 'UserName?',
         ]);
     }
+    /**
+     * @Route("/product/user", name="user_edit", methods={"PUT"})
+     */
+    // public function edit(Request $request, string $user): Response
+    // {
+    //     $entityManager = $this->getDoctrine()->getManager();
+    //     $user = $entityManager->getRepository(Product::class)->find($user);
+ 
+    //     if (!$product) {
+    //         return $this->json('Error :'.' No product found for id' . $id, 404);
+    //     }
+ 
+    //     $product->setName($request->request->get('name'));
+    //     $product->setDescription($request->request->get('description'));
+    //     $product->setPhoto($request->request->get('photo'));
+    //     $product->setPrice($request->request->get('price'));
+
+    //     $entityManager->flush();
+ 
+    //     $data =  [
+    //         'id' => $product->getId(),
+    //         'name' => $product->getName(),
+    //         'description' => $product->getDescription(),
+    //         'photo' => $product->getPhoto(),
+    //         'price' => $product->getPrice(),
+    //     ];
+         
+    //     return $this->json($data);
+    // }
 }
